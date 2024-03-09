e2a098caaf39dae511429a6428e971650501bdbb
h.kuwabara <h.kuwabara1103@gmail.com>
2024-03-09 18:44:49 +0900

    PhotoアイテムのComposableを実装

--- app/src/main/java/com/example/android_technique_collection/feature/searchphoto/SearchPhotoScreen.kt
+++ app/src/main/java/com/example/android_technique_collection/feature/searchphoto/SearchPhotoScreen.kt
@@ -1,10 +1,11 @@
 package com.example.android_technique_collection.feature.searchphoto
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.android_technique_collection.feature.searchphoto.component.PhotoThumbnailItem
 import com.example.android_technique_collection.feature.searchphoto.state.SearchPhotoViewState
 import com.example.android_technique_collection.ui.theme.Android_technique_collectionTheme
 
@@ -38,14 +40,20 @@ private fun SearchPhotoScreen(
     ) { padding ->
         when(uiState) {
             is SearchPhotoViewState.Shown -> {
-                Text(
-                    text = uiState.photos[0].imageUrl,
-                    modifier = Modifier.padding(padding)
-                )
+                LazyColumn {
+                    items(uiState.photos) { photo ->
+                        PhotoThumbnailItem(
+                            photo = photo,
+                            modifier = Modifier.padding(padding)
+                        )
+                    }
+                }
             }
             is SearchPhotoViewState.Failure -> {
                 Column(
-                    modifier = Modifier.fillMaxSize().padding(padding),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
@@ -56,7 +64,9 @@ private fun SearchPhotoScreen(
             }
             SearchPhotoViewState.Loading -> {
                 Column(
-                    modifier = Modifier.fillMaxSize().padding(padding),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
---
+++ app/src/main/java/com/example/android_technique_collection/feature/searchphoto/component/PhotoThumbnailItem.kt
@@ -0,0 +1,104 @@
+package com.example.android_technique_collection.feature.searchphoto.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.android_technique_collection.feature.searchphoto.state.Photo
+import com.example.android_technique_collection.ui.theme.Android_technique_collectionTheme
+
+@Composable
+fun PhotoThumbnailItem(
+    photo: Photo,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .heightIn(min = 200.dp)
+            .background(Color.Black),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.align(Alignment.Center)
+        )
+        AsyncImage(
+            model = photo.imageUrl,
+            contentDescription = photo.description,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black.copy(alpha = 0.8f))
+                .padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(fraction = 0.8f)
+            ) {
+                Text(
+                    text = photo.description ?: "No description",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(
+                    text = photo.photographer ?: "No photographer",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            Icon(
+                imageVector = Icons.Default.Favorite,
+                contentDescription = "likes",
+                tint = Color.Magenta,
+                modifier = Modifier.size(20.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = photo.likes.toString(),
+                color = Color.White,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PhotoThumbnailItemPreview() {
+    val photo = Photo(
+        photoId = "",
+        description = "Image description",
+        likes = 100,
+        imageUrl = "",
+        photographer = "Surface"
+    )
+    Android_technique_collectionTheme {
+        PhotoThumbnailItem(
+            photo = photo,
+            modifier = Modifier.height(180.dp)
+        )
+    }
+}
