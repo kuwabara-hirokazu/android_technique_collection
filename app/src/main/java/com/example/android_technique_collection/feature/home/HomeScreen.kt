72b355027665585dea694fc46b199ba6432615eb
kuwa <h.kuwabara1103@gmail.com>
2024-12-22 19:00:10 +0900

    ボタン色変えてみる

--- app/src/main/java/com/example/android_technique_collection/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/android_technique_collection/feature/home/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,6 +14,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.android_technique_collection.R
@@ -40,6 +42,12 @@ fun HomeScreen(
             Spacer(modifier = Modifier.height(40.dp))
             Button(
                 onClick = onNavigateToSearch,
+                colors = ButtonColors(
+                    containerColor = Color.Blue,
+                    contentColor = Color.White,
+                    disabledContainerColor = Color.Gray,
+                    disabledContentColor = Color.White
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
