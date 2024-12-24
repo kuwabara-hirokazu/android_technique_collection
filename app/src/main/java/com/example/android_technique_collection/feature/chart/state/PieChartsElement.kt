0da91eb211086042fd09aacaabf1e112a5a0746e
kuwa <h.kuwabara1103@gmail.com>
2024-12-24 23:32:01 +0900

    色変更

--- app/src/main/java/com/example/android_technique_collection/feature/chart/state/PieChartsElement.kt
+++ app/src/main/java/com/example/android_technique_collection/feature/chart/state/PieChartsElement.kt
@@ -27,7 +27,7 @@ data class PieChartsElement(
                 ),
                 PieChartsElement(
                     pieDegrees = calculatePieDegrees(10f),
-                    color = Color.DarkGray
+                    color = Color.Yellow
                 ),
             )
         }
